4e01a40d20d2ad4f846c05dd4b6dab43d5daf39a
gtwenty4 <56203735+gtwenty4@users.noreply.github.com>
2020-05-05 19:36:11 -0400

    Crear filtro global de excepciones (#6)
    Este commit agrega un filtro global de excepciones que sirve para
    manejar cualquier execepción que haya sido manejada o no por el
    desarrollador. Al mismo tiempo, se creo la carpeta app en donde se
    encuentran los archivos que al principio estaban en la carpeta src
    (excepto por el main.ts). Cuando se implemente el logger el filtro podra
    manejar una trazabilidad de los errores.

--- shopy-server/src/main.ts
+++ shopy-server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(3000);
 }
 bootstrap();
